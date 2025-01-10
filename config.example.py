017426d13f5c6d86e15ea2dddcb694489b13a8ac
KnownNight <kasich.nszar@gmail.com>
2025-01-10 12:27:26 +0200

    add README

--- config.example.py
+++ config.example.py
@@ -1,4 +1,4 @@
-#In the root directory, create an config.py file and copy the contents of the config.example.py file there.\
+#In the root directory, create an config.py file and copy the contents of the config.example.py file there.
 import os
 API_TOKEN = "Your telegram bot token"
 DATABASE_URL = os.getenv("DATABASE_URL", "connection string to postgress")
